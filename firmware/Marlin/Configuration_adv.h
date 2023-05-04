d8510fc658c724f95489c3cb0174786062f07f07
Fabian Weber <fweber@schutzwerk.com>
2023-05-04 09:09:33 +0200

    Restore M114 legacy behavior and fix hardware setup config

--- firmware/Marlin/Configuration_adv.h
+++ firmware/Marlin/Configuration_adv.h
@@ -3781,8 +3781,8 @@
 
 // Extra options for the M114 "Current Position" report
 //#define M114_DETAIL         // Use 'M114` for details to check planner calculations
-//#define M114_REALTIME       // Real current position based on forward kinematics
-//#define M114_LEGACY         // M114 used to synchronize on every call. Enable if needed.
+#define M114_REALTIME       // Real current position based on forward kinematics
+#define M114_LEGACY         // M114 used to synchronize on every call. Enable if needed.
 
 //#define REPORT_FAN_CHANGE   // Report the new fan speed when changed by M106 (and others)
 
@@ -4389,7 +4389,7 @@
   #define PROBOTER_PROBING_SPEED 1000
 
   // Light configuration
-  #if ENABLED(PROBE_1)
+  #if ENABLED(PROBE_2)
     #define PROBOTER_IS_LIGHT_CONTROLLER
   #endif
   // The light control pin
